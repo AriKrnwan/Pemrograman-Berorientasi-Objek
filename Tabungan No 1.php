6e06a513953f701e77f75d57ace0345f4e0d48b2
AriKrnwan <72693634+AriKrnwan@users.noreply.github.com>
2021-11-01 23:34:38 +0700

    Add files via upload

---
+++ Tabungan No 1.php
@@ -0,0 +1,39 @@
+<!-- Nama   : Ari Kurniawan -->
+<!-- NIM    : 20051397057 -->
+<!-- Kelas  : Manajemen Informatika 2020A -->
+<!-- Praktikum 3 No1 Tabungan -->
+
+<?php
+class Tabungan{
+    protected $saldo;
+}
+
+class PengambilanUang extends Tabungan{
+    private $proteksi;
+
+    public function __construct($saldo){
+        $this->saldo = $saldo;
+        $this->proteksi = 1000;
+    }
+
+    public function getSaldo(){
+        return 'Uang yang ditabung : '. $this->saldo. '<br> Uang yang diproteksi : '. $this->proteksi;
+    }
+
+    public function ambilUang($jumlah){
+        if ($jumlah === 4500) {
+            return 'Uang yang akan diambil : '. $jumlah. ' false <br> Saldo sekarang : '. $this->saldo;
+        } else {
+            return 'Uang yang akan diambil : '. $jumlah. ' true  <br> Saldo Sekarang : '. $this->saldo -= $jumlah;
+        }
+    }
+}
+
+$tabungan = new PengambilanUang(5000);
+echo $tabungan->getSaldo();
+echo '<br> -------------------- <br>';
+echo $tabungan-> ambilUang(4500);
+echo '<br> -------------------- <br>';
+echo $tabungan-> ambilUang(2500);
+
+?>
